426796bc41c7ead78c5fc761c81ba2b0c44c26dc
luisrincons <luisrincons@gmail.com>
2014-11-21 20:53:07 -0600

    v1.6

--- js/index.js
+++ js/index.js
@@ -1,21 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+//index.js
 var app = {
     // Application Constructor
     initialize: function() {
@@ -34,27 +17,19 @@ var app = {
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
         var pushNotification = window.plugins.pushNotification;
-        if (device.platform == 'android' || device.platform == 'Android') {
-            alert("Register called");
-            //tu Project ID aca!!
-            pushNotification.register(this.successHandler, this.errorHandler,{"senderID":"992099932035","ecb":"app.onNotificationGCM"});
-        }
-        else {
-            alert("Register called");
-            pushNotification.register(this.successHandler,this.errorHandler,{"badge":"true","sound":"true","alert":"true","ecb":"app.onNotificationAPN"});
-        }
+        pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"992099932035","ecb":"app.onNotificationGCM"});
+
+    },
+       receivedEvent: function(id) {
+        var parentElement = document.getElementById(id); 
+        var listeningElement = parentElement.querySelector('.listening'); 
+        var receivedElement = parentElement.querySelector('.received'); 
+
+        listeningElement.setAttribute('style', 'display:none;'); 
+        receivedElement.setAttribute('style', 'display:block;');
+		
+        console.log('Received Event: ' + id);
     },
     // result contains any message sent from the plugin call
     successHandler: function(result) {
@@ -71,38 +46,22 @@ var app = {
                 {
                     console.log("Regid " + e.regid);
                     alert('registration id = '+e.regid);
-                    //Cuando se registre le pasamos el regid al input
-                    document.getElementById('regId').value = e.regid;
                 }
-            break;
- 
+                break;
+
             case 'message':
-              // NOTIFICACION!!!
-              alert('message = '+e.message+' msgcnt = '+e.msgcnt);
-            break;
- 
+                // this is the actual push notification. its format depends on the data model from the push server
+                alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+                break;
+
             case 'error':
-              alert('GCM error = '+e.msg);
-            break;
- 
+                alert('GCM error = '+e.msg);
+                break;
+
             default:
-              alert('An unknown GCM event has occurred');
-              break;
-        }
-    },
-    onNotificationAPN: function(event) {
-        var pushNotification = window.plugins.pushNotification;
-        alert("Running in JS - onNotificationAPN - Received a notification! " + event.alert);
-        
-        if (event.alert) {
-            navigator.notification.alert(event.alert);
-        }
-        if (event.badge) {
-            pushNotification.setApplicationIconBadgeNumber(this.successHandler, this.errorHandler, event.badge);
-        }
-        if (event.sound) {
-            var snd = new Media(event.sound);
-            snd.play();
+                alert('An unknown GCM event has occurred');
+                break;
         }
     }
+
 };
