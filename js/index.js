3a56e669bf3f71e2ca0a885556a3ad6976d3e6c5
luisrincons <luisrincons@gmail.com>
2014-11-21 23:30:38 -0600

    v2.0

--- js/index.js
+++ js/index.js
@@ -44,7 +44,6 @@ var app = {
             case 'registered':
                 if ( e.regid.length > 0 )
                 {
-                    alert("ID:" + document.getElementById('regId').value);
                     console.log("Regid " + e.regid);
                     alert('registration id = '+e.regid);
                     //Cuando se registre le pasamos el regid al input 
